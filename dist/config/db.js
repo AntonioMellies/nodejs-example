e243bc5335c9ffc434c587b3b9b0b19dba9d8104
AntonioMellies <neto-fred@live.com>
2020-03-01 13:56:43 -0300

    config database with docker

--- dist/config/db.js
+++ dist/config/db.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 class DataBase {
     constructor() {
-        this.DB_URL = "mongodb://mongodb-service/db_portal";
+        this.DB_URL = "mongodb://localhost:27017/db_portal";
     }
+    //private DB_URL = "mongodb://mongodb-service/db_portal";
     createConnection() {
         mongoose.connect(this.DB_URL);
     }
